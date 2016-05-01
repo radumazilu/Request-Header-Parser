f570f2890b80e995495388b61b7bc27e9a397a16
Radu Mazilu <radumazilu@Radus-MacBook-Pro.local>
2016-05-01 17:01:54 +0300

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+var http = require('http')
+var express = require('express')
+var accepts = require('accepts');
+var useragent = require('useragent');
+var app = express()
+var port = Number(process.env.PORT || 3000)
+
+var os = require( 'os' );
+
+app.get('/translation', function(request, response) {
+    var contentType = response.getHeader('content-type');
+    var agent = useragent.parse(request.headers['user-agent']);
+    response.json({
+      ip: request.headers['x-forwarded-for'] || request.connection.remoteAddress,
+      language: accepts(request).languages()[0],
+      software: agent.os.toString()
+    })
+});
+
+app.listen(port)
